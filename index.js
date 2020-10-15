c94fa49f15f7e9ad388b6d040952fd91ce36b47e
Akbor5374 <akborhossain53741@gmail.com>
2020-10-15 23:51:59 +0600

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs  = require('fs-extra');
+const fs = require('fs-extra');
 const ObjectId = require('mongodb').ObjectId
 const MongoClient = require('mongodb').MongoClient;
 const fileUpload = require('express-fileupload');
@@ -30,95 +30,82 @@ client.connect(err => {
         const file = req.files.file;
         const title = req.body.title;
         const description = req.body.description;
-        // const filePath = `${__dirname}/images/${file.name}`;
+        const newImg = file.data;
+        const encodeImg = newImg.toString('base64');
+        const image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encodeImg, 'base64')
+        };
 
-        // file.mv(filePath, err => {
-        //     if (err) {
-        //         res.status(500).send({ msg: "Failed to uploaded image" });
-        //     }
-            const newImg = file.data;
-            const encodeImg = newImg.toString('base64');
-            const image = {
-                contentType: file.mimetype,
-                size: file.size,
-                img: Buffer.from(encodeImg, 'base64')
-            };
-
-            serviceCollection.insertOne({ title, description, img:image })
+        serviceCollection.insertOne({ title, description, img: image })
             .then(result => {
-                // fs.remove(filePath, err => {
-                //     if(err){
-                //      console.log(err);
-                //      res.status(500).send({ msg: "Failed to uploaded image" });
-                //     }
-                    res.send(result.insertedCount > 0);
-                // });
+                res.send(result.insertedCount > 0);
             });
-        // });
     });
 
     app.get('/getAllService', (req, res) => {
         serviceCollection.find({})
-        .toArray( (error, documents) => {
-            res.send(documents);
-        })
+            .toArray((error, documents) => {
+                res.send(documents);
+            })
     });
 
     app.get('/getSingleService/:id', (req, res) => {
         const id = req.params.id;
-        serviceCollection.find({_id: ObjectId(id)})
-        .toArray( (error, documents) => {
-            res.send(documents[0]);
-        })
+        serviceCollection.find({ _id: ObjectId(id) })
+            .toArray((error, documents) => {
+                res.send(documents[0]);
+            })
     });
 
     app.post('/addOrderService', (req, res) => {
         orderServiceCollection.insertOne(req.body)
-        .then((result) => {
-            res.send(result.insertedCount > 0);
-        })
+            .then((result) => {
+                res.send(result.insertedCount > 0);
+            })
     });
 
     app.get('/getOrderService', (req, res) => {
         const email = req.query.email;
-        orderServiceCollection.find({email: email})
-        .toArray( (err, documents) => {
-            res.send(documents);
-        })
+        orderServiceCollection.find({ email: email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     });
 
-    app.post('/addAdmin', (req, res)  => {
+    app.post('/addAdmin', (req, res) => {
         adminCollection.insertOne(req.body)
-        .then(result => {
-            res.send(result.insertedCount > 0);
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
     });
     app.post('/checkAdmin', (req, res) => {
-        adminCollection.find({email: req.body.email})
-        .toArray( (err, documents) => {
-            res.send(documents.length > 0);
-        })
+        adminCollection.find({ email: req.body.email })
+            .toArray((err, documents) => {
+                res.send(documents.length > 0);
+            })
     });
 
     app.get('/getAllUserService', (req, res) => {
         orderServiceCollection.find({})
-        .toArray( (err, documents) => {
-            res.send(documents);
-        })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     });
 
     app.post('/addReview', (req, res) => {
         reviewCollection.insertOne(req.body)
-        .then(result => {
-            res.send(result.insertedCount > 0)
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
     });
 
     app.get('/getAllReview', (req, res) => {
         reviewCollection.find({})
-        .toArray( (err, documents) => {
-            res.send(documents);
-        })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     });
 
 });
